edfdf0a1c381858778f4163473f731b053d5fa0c
Shruti Swarupa Dhar <dharreny@gmail.com>
2024-07-26 12:09:48 +0530

    uploaded test images and modified prompt

--- app.py
+++ app.py
@@ -28,16 +28,18 @@ def upload_to_gemini(data, mime_type=None):
 
 system_prompt =  '''
 As a highly trained medical practioner in image analysis, you are responsible for generating an approximate diagnosis 
-so that using that info patient can report for further doctor consultation.
+so that using that info patient can report for further doctor consultation. You will just detect the disease and any abnormal anomalies
+from the image ans suggest what further steps can be done. Do not directly say to seek medical advice rather give an observation first 
+and then recommend to a doctor.
 
-Your ressponsibilities include:
-1.Detailed analysis: carefully analyse the image, identifying any abnormal findings.
-2.Structured report: identify all anomalies or signs of disease and curate all findings in a structured manner in form of a report.
+Your should give me the following:
+1.Detailed analysis: carefully analyse the image to identify any abnormal findings. Surf the web for possible disease matches.
+2.Structured report: identify all anomalies or common signs of disease and curate all findings in a structured manner in form of a report.
 3.Recommendations: Based on your findings, recommend certain further steps or practices to follow.
-4. Treatment: If appropiate, provide treatment.
+4.Treatment: If appropiate, provide treatment or advice for doctor consultation.
 
 Important note:
-1. Scope: respond only to images that are related to human illness
+1. Scope: Do respond to images with skin abnormalities that are related to human illness
 2. Clarity: if image clarity is compromised respond saying that "Sorry! I cannot process on the image due to limited visibility"
 3. Disclaimer : I'm an AI agent and donot hold the entire knowledge to give accurate analysis. You should consult a physician.
 '''
@@ -46,39 +48,35 @@ st.set_page_config(page_title="MediBuddy", page_icon=":material/thumb_up:", layo
 })
 
 st.image("logo.png", width = 450)
-st.subheader(':gray[An application to help you diagnose your disease easy and quick !]' )
-uploaded_file = st.file_uploader("Kindky upload the medical image with visible symptoms")
+st.subheader(':gray[An assistant to help you diagnose !]' )
+uploaded_file = st.file_uploader("Kindy upload the medical image with visible symptoms")
 submit_button = st.button(label="Generate analysis")
 
-if uploaded_file is not None:
+if uploaded_file is not None:  
     # Create a temporary directory
-    with tempfile.NamedTemporaryFile(delete=False) as temp_file:
-        # Save the uploaded file to the temporary directory
-        temp_file.write(uploaded_file.read())
-        temp_file_path = temp_file.name
+  with tempfile.NamedTemporaryFile(delete=False) as temp_file:
+    # Save the uploaded file to the temporary directory
+    temp_file.write(uploaded_file.read())
+    temp_file_path = temp_file.name
+    st.image(uploaded_file)
 
 if submit_button:
-    image_data=uploaded_file.getvalue()
-
     files = [
     upload_to_gemini(temp_file_path, mime_type="image/jpeg"),
     ]
 
     chat_session = model.start_chat(
-  history=[
+    history=[
     {
       "role": "user",
       "parts": [
         files[0],
-        "what is this disease",
+        system_prompt,
       ],
-    },
-    {
-      "role": "model",
-      "parts": system_prompt,
     },
     ]
     )
     response = chat_session.send_message("INSERT_INPUT_HERE")
+    # response = model.generate_content(system_prompt)
 
     st.write(response.text)
